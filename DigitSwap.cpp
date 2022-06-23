73826f80925317c0ca249de10ba6d1772437cb8a
AlparslanDuru <81374685+AlparslanDuru@users.noreply.github.com>
2022-06-22 23:25:35 -0500

    Update DigitSwap.cpp

--- DigitSwap.cpp
+++ DigitSwap.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main()
 {
-    int x;
-    int y;
-    int z;
+    int x, y, z;
     cin >> x;
+    
     if(x>10 && x<100){
-    if (x%10!=0){
-        y = x%10;
-        z = x/10;
-    }
+        if (x%10!=0){
+            y = x%10;
+            z = x/10;
+        }
     cout << y << z;
     }
+    
     return 0;
 }
