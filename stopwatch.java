514a8da1b17b4091cb50f218213c7a018ad20df5
AlparslanDuru <81374685+AlparslanDuru@users.noreply.github.com>
2023-01-01 22:58:35 -0600

    Difficulty 1.4

--- stopwatch.java
+++ stopwatch.java
@@ -8,19 +8,18 @@ public class stopwatch
 		
 		n = scan.nextInt();
 		
-        int arr[] = new int[n];
+        	int arr[] = new int[n];
         
-        for(int i=0; i<n; i++)
-            arr[i]= scan.nextInt();
+        	for(int i=0; i<n; i++)
+            		arr[i]= scan.nextInt();
 
-        if(n%2 == 1)
-            System.out.println("still running");
-        else
-        {
-            for (int i=0; i<n/2; i++)
-                count += arr[i*2 + 1] - arr[i*2];
-            
-            System.out.println(count);
-        }
-    }
+        	if(n%2 == 1)
+            		System.out.println("still running");
+        	else
+        	{
+            		for (int i=0; i<n/2; i++)
+                	count += arr[i*2 + 1] - arr[i*2];
+            		System.out.println(count);
+        	}
+    	}
 }
