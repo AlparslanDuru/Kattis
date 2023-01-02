3cc4dec73c6eb0e45d5cddc72c0003847efe4609
AlparslanDuru <81374685+AlparslanDuru@users.noreply.github.com>
2023-01-01 23:00:01 -0600

    Create dicecup.java

---
+++ dicecup.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        int n, m, lowest;
+        
+        n = scan.nextInt();
+        m = scan.nextInt();
+        
+        if(n > m)
+            lowest = m + 1;
+        else
+            lowest = n + 1;
+        
+        System.out.println(lowest);
+        
+        for(int i = 1; i <= Math.abs(n-m); i++)
+            System.out.println(lowest+i);
+    
+    }
+}
