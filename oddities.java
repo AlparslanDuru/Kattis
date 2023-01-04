09afa08abf421359d08e91d3353bcb2b59093168
AlparslanDuru <81374685+AlparslanDuru@users.noreply.github.com>
2023-01-03 23:21:31 -0600

    Create oddities.java

---
+++ oddities.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        int n, x;
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+        
+        for (int i=0; i<n; i++)
+        {
+            x = scan.nextInt();
+            if (x%2==0)
+                System.out.println(x + " is even");
+            else
+                System.out.println(x + " is odd");
+
+        }
+    }
+}
