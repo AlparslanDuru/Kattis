563398ac751f80e2c75c0eb43bebcbbe15d0b164
AlparslanDuru <81374685+AlparslanDuru@users.noreply.github.com>
2023-01-04 23:36:19 -0600

    Difficulty 1.4

--- pet.java
+++ pet.java
@@ -5,30 +5,25 @@ class pet{
     public static void main(String[] args){
         
         int n=0, max=0;
-        int[] arr = new int[5];
+        Integer[] arr = {0, 0, 0, 0, 0};
         
         Scanner scan = new Scanner(System.in);
         
         for (int i=0; i<5; i++)
         {
             for (int j=0; j<4; j++)
-            {
                 n += scan.nextInt();
-            }
+                
             arr[i] = n;
             n=0;
         }
         
         for (int i : arr)
-        {
             if (i>max)
-            {
                 max = i;
-            }
-        }
         
         int getArrayIndex = Arrays.asList(arr).indexOf(max);
 
-        System.out.println(getArrayIndex + " " + max);
+        System.out.println(getArrayIndex + 1 + " " + max);
     }
 }
