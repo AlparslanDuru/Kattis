a7a5d6321ebc49c87a5ac5c5c5f917ce32cfc205
AlparslanDuru <81374685+AlparslanDuru@users.noreply.github.com>
2023-01-04 23:06:03 -0600

    Difficulty 1.4

--- ratingproblems.java
+++ ratingproblems.java
@@ -1,29 +1,22 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-class Main{
+class ratingproblems{
     public static void main(String[] args){
         
         Scanner scan = new Scanner(System.in);
         int n, k, num=0;
-        double average, minRating, maxRating;
-        
+        double  minRating, maxRating;
+    
         
         n = scan.nextInt();
         k = scan.nextInt();
         
-        int[] arr = new int[k];
-        
         for (int i=0; i<k; i++)
-        {
-            arr[i] = scan.nextInt();
-            num += arr[i];
-        }
+            num += scan.nextInt();
         
-        average = num/k*1.0;
-        
-        minRating = (average + ((-3*(n-k)) / (n-k) * 1.0) ) / n*1.0;
-        maxRating = (average + (( 3*(n-k)) / (n-k) * 1.0) ) / n*1.0;
+        minRating = (num + -3 * (n - k)) / (n * 1.0);
+        minRating = (num + 3 * (n - k)) / (n * 1.0);
         
         System.out.println(minRating + " " + maxRating);
     }
