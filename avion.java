6eabbe2cfd752934184aa4e05058f8279ebb2252
AlparslanDuru <81374685+AlparslanDuru@users.noreply.github.com>
2023-01-07 23:45:06 -0600

    Difficulty 1.4

--- avion.java
+++ avion.java
@@ -1,11 +1,25 @@
 import java.util.Scanner;
-import java.lang.Math;
+import java.lang.String;
 
 public class avion
 {
     public static void main(String[] args) {
         
-
+        String str;
+        int counter=0;
+        Scanner scan = new Scanner(System.in);
+        for (int i=1; i<6; i++)
+        {
+            str = scan.nextLine();
+            if (str.contains("FBI"))
+            {
+                System.out.println(i);
+                counter++;
+            }
+        }
+        
+        if (counter==0)
+            System.out.println("HE GOT AWAY!");
         
     }
 }
